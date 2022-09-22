b100b104deb9ca600863a33ab596f2e74789fd6c
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-22 19:33:44 +0530

    Added UC1 Creating stack

--- src/com/bridgelabz/StackMain.java
+++ src/com/bridgelabz/StackMain.java
@@ -4,12 +4,12 @@ public class StackMain {
     public static void main(String[] args) {
         System.out.println("Stacks and Queues program");
 
-        //StackOperations stackOperations = new StackOperations();
-        //stackOperations.push(70);
-        //stackOperations.push(30);
-        //stackOperations.push(56);
+        StackOperations stackOperations = new StackOperations();
+        stackOperations.push(70);
+        stackOperations.push(30);
+        stackOperations.push(56);
 
-        //stackOperations.show();
+        stackOperations.show();
 
 
     }
