ea1f02525bc0a3782bc6018fad5d268e87836857
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-09-22 19:31:40 +0530

    Started Stak and Queue programm Computation

---
+++ src/com/bridgelabz/StackMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class StackMain {
+    public static void main(String[] args) {
+        System.out.println("Stacks and Queues program");
+
+        //StackOperations stackOperations = new StackOperations();
+        //stackOperations.push(70);
+        //stackOperations.push(30);
+        //stackOperations.push(56);
+
+        //stackOperations.show();
+
+
+    }
+}
